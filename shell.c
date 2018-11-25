5bc83398e05821eb19baa4ab8b363c65f81f9a7f
Aaron Li <aaronli39@gmail.com>
2018-11-25 10:35:30 -0500

    worked on path

--- shell.c
+++ shell.c
@@ -6,6 +6,7 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <pwd.h>
 
 char **parse_args(char *line) {
     char **args = calloc(sizeof(char *), 6);
@@ -52,18 +53,32 @@ void execute(char **argv) {
     }
 }
 
+void getPath() {
+    char *ret = calloc(sizeof(char *), 100);
+    char path[FILENAME_MAX];
+    getcwd(path, FILENAME_MAX);
+    char temp[1000];
+    strcpy(temp, path);
+    printf("\n%d\n", temp[5]);
+    int index = 0;
+}
+
 void main(void) {
     // store input
     char line[500];             
     // args
     char *argv[100];              
-    
+    char host_name[64];
+    gethostname(host_name,64);
+    char path[FILENAME_MAX];
+    getcwd(path, FILENAME_MAX);
     while (1) {               
         // char * pter = malloc(sizeof(size));
         // buf = getcwd(pter, sizeof(size));
         // printf("path: %s\n", buf);
         // cmd line prompt    
-        printf("> ");     
+        printf("[%s]%s$ ",host_name, path);
+        getPath();
         gets(line);
         parse(line, argv);       
         // if exit exit, otherwise execute
@@ -73,3 +88,28 @@ void main(void) {
         printf("\n");
     }
 }
+
+// int main(){
+//   char * input;
+//   char ** args;
+//   int status = 1;
+//   char * username = getpwuid(getuid())->pw_name;
+//   char host_name[64];
+//   gethostname(host_name,64);
+
+//   while(status){
+//     printf("%s@%s: $ ",username,host_name);
+//     input = read_input();
+//     char * cur_input = input;
+//     strsep(&input,";");
+//     while (cur_input){
+//       //printf("Your input now is cur:%s or input:%s\n",cur_input,input);
+//       args = parse_args(cur_input);
+//       //print_list(args);
+//       status = execute_args(args);
+//       cur_input = input;
+//       strsep(&input,";");
+//     }
+//   }
+//   return 0;
+// }
