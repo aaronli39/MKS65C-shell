2dc906f751f0338bcfc492bfc1475ea43cb84cea
Aaron Li <aaronli39@gmail.com>
2018-12-02 19:26:28 -0500

    got cd to work

--- shell.c
+++ shell.c
@@ -19,113 +19,135 @@ void parse_args(char *line, char **argv) {
 
 void parse(char *line, char **argv) {
     // if not end of line
-    while (*line != '\0') {     
-        // replace white spaces with 0    
+    while (*line != '\0') {
+        // replace white spaces with 0
         while (*line == ' ')
-            *line++ = '\0';     
+        *line++ = '\0';
         // save the argument position
-        *argv++ = line;          
+        *argv++ = line;
         while (*line != '\0' && *line != ' ')
-            // skip the argument until ... 
-            line++;             
+        // skip the argument until ...
+        line++;
     }
-    // end of argument list 
-    *argv = '\0';                 
+    // end of argument list
+    *argv = '\0';
 }
 
 void execute(char **argv) {
     int status;
     int child = fork();
 
-    if (child < 0) {     
+    if (child < 0) {
         // fork child to do work ;-;
         printf("Error: %s\n", strerror(errno));
         exit(1);
-    } else if (child == 0) {          
+    } else if (child == 0) {
         // child: execute command
-        if (execvp(*argv, argv) < 0) {   
-            printf("%s\n", strerror(errno));
+        if (execvp(*argv, argv) < 0) {
+            // printf("%s\n", strerror(errno));
             exit(1);
         }
-    } else {                                
+    } else {
         // parent: wait till complete
-        wait(&status);     
+        wait(&status);
     }
 }
 
-void getDir(char out[]){
-    char path[1024] = "";
+void getDir(char out[]) {
+    char path[1024];
     if (getcwd(path, 1024) == NULL){
         printf("Can't get directory\n");
         exit(1);
     } char *home = getenv("HOME");
 
     // if cwd is subdir of home
-    if (strstr(path,home)){
-        strcpy(out,"~");
-        strcat(out,path+strlen(home));
+    if (strstr(path, home)){
+        strcpy(out, "~");
+        strcat(out, path + strlen(home));
     }
     // if cwd not in home
     else{
-        strcpy(out,path);
+        strcpy(out, path);
     }
 }
 
 // int main() {
 //     // store input
-//     char line[500];             
+//     char line[500];
 //     // args
-//     char *argv[100];              
+//     char *argv[100];
 //     char host_name[64];
 //     gethostname(host_name,64);
 //     char path[FILENAME_MAX];
 //     getcwd(path, FILENAME_MAX);
-//     while (1) {               
+//     while (1) {
 //         // char * pter = malloc(sizeof(size));
 //         // buf = getcwd(pter, sizeof(size));
 //         // printf("path: %s\n", buf);
-//         // cmd line prompt    
+//         // cmd line prompt
 //         printf("[%s]%s$ ",host_name, path);
 //         // getPath();
 //         gets(line);
-//         parse(line, argv);       
+//         parse(line, argv);
 //         // if exit exit, otherwise execute
-//         if (strcmp(argv[0], "exit") == 0)  
-//         exit(0);          
-//         execute(argv);        
+//         if (strcmp(argv[0], "exit") == 0)
+//         exit(0);
+//         execute(argv);
 //         printf("\n");
 //     }
 // }
 
 int main() {
-    // store input
-    char line[500];             
-    char *current;
-    
-    char *argv[100];              
-    char host_name[64];
+    char line[500]; // input line
+    char *current; // current command to execute
+    char *argv[100]; // stores arguments
+    char host_name[64]; // hostname
     gethostname(host_name,64);
-    char cwd[1024];
+    char cwd[1024]; // store cwd
     getDir(cwd);
-    while (1) {               
+
+    while (1) {
         // char * pter = malloc(sizeof(size));
         // buf = getcwd(pter, sizeof(size));
         // printf("path: %s\n", buf);
-        // cmd line prompt    
+        // cmd line prompt
         printf("[%s]%s$ ",host_name, cwd);
         gets(line);
 
         // run first part of semicolons
         char *args = line;
-        current = strsep(&args, ";");
-        printf("temp: %s\n", current);
-        current = strsep(&args, ";");
-        printf("next one: %s\n", current);
-        parse(line, argv);       
-        // if exit exit, otherwise execute
-        if (strcmp(argv[0], "exit") == 0)  
-            exit(0);          
-        execute(argv);        
-        printf("\n");
+        while ((current = strsep(&args, ";")) != NULL && strcmp(current, "") != 0) {
+            // printf("-------executing:%s---------\n", current);
+            parse(current, argv);
+
+            // if cd
+            if (strcmp(argv[0], "cd") == 0) {
+                // no args(home dir)
+                if (argv[1] == NULL || strcmp(argv[1], "~") == 0){
+                    char * home;
+                    chdir((home = getenv("HOME")));
+                }
+                // 1 arg
+                else if (argv[2] == NULL && argv[1] != NULL){
+                    if (chdir(argv[1]) != 0){
+                        printf("%s\n", strerror(errno));
+                    }
+                }
+                // error if more than 1 arg
+                else {
+                    printf("Error: cd: Too many arguments\n");
+                }
+                // refresh cwd
+                getDir(cwd);
+            }
+            // char temp[50];
+            // strcpy(temp, (char *) argv[1]);
+            // printf("<%s>\n", temp);
+            // printf("argv: %s\n", *argv);
+            // if exit exit, otherwise execute
+            if (strcmp(argv[0], "exit") == 0 || current == NULL) {
+                exit(0);
+            } execute(argv);
+        }
     } return 0;
 }
