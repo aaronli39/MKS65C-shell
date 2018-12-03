db85aa985497a838ff33686917b3763d79217fb0
Aaron Li <aaronli39@gmail.com>
2018-12-02 19:42:48 -0500

    fixed parsing bug

--- shell.c
+++ shell.c
@@ -9,19 +9,35 @@
 #include <pwd.h>
 
 void parse_args(char *line, char **argv) {
-    char **args = calloc(sizeof(char *), 10);
-    char *token;
-    printf("ORIGINAL STRING:\n%s\n", line);
-    while ((token = strsep(&line, ";")) != NULL) {
-        printf("parsed: %s\n", token);
+    //Remove whitespace in front of input
+    while (*line == ' ' || *line == '\t'){
+        line++;
     }
+    char *prev = NULL;
+    int index = 0;
+
+    while (line) {
+        prev = line;
+        strsep(&line, " ");
+        int num_special = 0;
+        //Checks for special characters and adds them into the string array
+        // num_special += sep_special(old,&output,">>",&arg_index);
+        // num_special += sep_special(old,&output,">",&arg_index);
+        // num_special += sep_special(old,&output,"<",&arg_index);
+        // num_special += sep_special(old,&output,"|",&arg_index);
+        //If there was not anything special about the current arg, then just add it
+        if (num_special < 1 && *prev){
+            argv[index] = prev;
+            index++;
+        }
+    } argv[index] = NULL; // add ending null
 }
 
 void parse(char *line, char **argv) {
     // if not end of line
     while (*line != '\0') {
         // replace white spaces with 0
-        while (*line == ' ')
+        while (*line == ' ' || *line == '\t')
         *line++ = '\0';
         // save the argument position
         *argv++ = line;
@@ -71,32 +87,6 @@ void getDir(char out[]) {
     }
 }
 
-// int main() {
-//     // store input
-//     char line[500];
-//     // args
-//     char *argv[100];
-//     char host_name[64];
-//     gethostname(host_name,64);
-//     char path[FILENAME_MAX];
-//     getcwd(path, FILENAME_MAX);
-//     while (1) {
-//         // char * pter = malloc(sizeof(size));
-//         // buf = getcwd(pter, sizeof(size));
-//         // printf("path: %s\n", buf);
-//         // cmd line prompt
-//         printf("[%s]%s$ ",host_name, path);
-//         // getPath();
-//         gets(line);
-//         parse(line, argv);
-//         // if exit exit, otherwise execute
-//         if (strcmp(argv[0], "exit") == 0)
-//         exit(0);
-//         execute(argv);
-//         printf("\n");
-//     }
-// }
-
 int main() {
     char line[500]; // input line
     char *current; // current command to execute
@@ -118,8 +108,8 @@ int main() {
         char *args = line;
         while ((current = strsep(&args, ";")) != NULL && strcmp(current, "") != 0) {
             // printf("-------executing:%s---------\n", current);
-            parse(current, argv);
-
+            parse_args(current, argv);
+            printf("xd: %d\n", ' ');
             // if cd
             if (strcmp(argv[0], "cd") == 0) {
                 // no args(home dir)
